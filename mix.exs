fc5cdb3415c2005f88993b342f9ac8087f3dad90
Tyr Chen <tyr.chen@gmail.com>
2020-11-29 14:57:42 -0800

    update license

--- mix.exs
+++ mix.exs
@@ -1,13 +1,26 @@
 defmodule JsonDataFaker.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
   def project do
     [
       app: :json_data_faker,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "JsonDataFaker",
+      docs: [
+        extras: ["README.md"]
+      ],
+      source_url: "https://github.com/tyrchen/json_data_faker",
+      homepage_url: "https://github.com/tyrchen/json_data_faker",
+      description: """
+      Build API routes based on OpenAPI v3 spec.
+      """,
+      package: package()
     ]
   end
 
@@ -25,6 +38,18 @@ defmodule JsonDataFaker.MixProject do
       {:randex, "~> 0.4.0"},
       {:faker, "~> 0.16"},
       {:uuid, "~> 1.1"}
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "../LICENSE*"],
+      licenses: ["MIT"],
+      maintainers: ["tyr.chen@gmail.com"],
+      links: %{
+        "GitHub" => "https://github.com/tyrchen/json_data_faker",
+        "Docs" => "https://hexdocs.pm/json_data_faker"
+      }
     ]
   end
 end
