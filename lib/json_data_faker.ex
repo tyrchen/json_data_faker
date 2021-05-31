59f4e22f783d4f7713f8bbe9d2dc32cfef9dc101
Alberto Sartori <alberto.sartori.as@gmail.com>
2021-05-31 16:08:59 +0200

    handle allOf

--- lib/json_data_faker.ex
+++ lib/json_data_faker.ex
@@ -61,6 +61,12 @@ defmodule JsonDataFaker do
   defp generate_by_type(%{"anyOf" => anyOf}, root, opts),
     do: anyOf |> Enum.map(&generate_by_type(&1, root, opts)) |> StreamData.one_of()
 
+  defp generate_by_type(%{"allOf" => allOf}, root, opts) do
+    allOf
+    |> merge_all_of(root)
+    |> generate_by_type(root, opts)
+  end
+
   defp generate_by_type(%{"enum" => choices}, _root, _opts), do: StreamData.member_of(choices)
 
   defp generate_by_type(%{"type" => "boolean"}, _root, _opts), do: boolean()
@@ -213,6 +219,52 @@ defmodule JsonDataFaker do
   defp stream_gen(fun) do
     StreamData.map(StreamData.constant(nil), fn _ -> fun.() end)
   end
+
+  defp merge_all_of(all_ofs, root) do
+    all_of_merger_root = fn root -> &all_of_merger(&1, &2, &3, root) end
+
+    Enum.reduce(all_ofs, %{}, fn all_of, acc ->
+      Map.merge(acc, resolve(all_of, root), all_of_merger_root.(root))
+    end)
+  end
+
+  defp all_of_merger(key, v1, v2, _root)
+       when key in ["minLength", "minProperties", "minimum", "maxItems"],
+       do: max(v1, v2)
+
+  defp all_of_merger(key, v1, v2, _root)
+       when key in ["maxLength", "maxProperties", "maximum", "minItems"],
+       do: min(v1, v2)
+
+  defp all_of_merger(key, v1, v2, _root)
+       when key in ["uniqueItems", "exclusiveMaximum", "exclusiveMinimum"],
+       do: v1 or v2
+
+  defp all_of_merger("multipleOf", v1, v2, _root) do
+    # TODO fix
+    case Integer.gcd(v1, v2) do
+      1 -> v1 * v2
+      _ -> max(v1, v2)
+    end
+  end
+
+  defp all_of_merger("enum", v1, v2, _root), do: Enum.filter(v1, &(&1 in v2))
+
+  defp all_of_merger("required", v1, v2, _root), do: v1 |> Enum.concat(v2) |> Enum.uniq()
+
+  defp all_of_merger(_property, %{"$ref" => _} = v1, %{"$ref" => _} = v2, root) do
+    f1 = resolve(v1, root)
+    f2 = resolve(v2, root)
+    all_of_merger_root = fn root -> &all_of_merger(&1, &2, &3, root) end
+    Map.merge(f1, f2, all_of_merger_root.(root))
+  end
+
+  defp all_of_merger(_key, m1, m2, root) when is_map(m1) and is_map(m2) do
+    all_of_merger_root = fn root -> &all_of_merger(&1, &2, &3, root) end
+    Map.merge(m1, m2, all_of_merger_root.(root))
+  end
+
+  defp all_of_merger(_key, _v1, v2, _root), do: v2
 
   defp resolve(%{"$ref" => ref}, root), do: ExJsonSchema.Schema.get_fragment!(root, ref)
   defp resolve(schema, _root), do: schema
